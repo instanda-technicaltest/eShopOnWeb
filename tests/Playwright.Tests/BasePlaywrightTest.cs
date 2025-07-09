efb901df8c39b4b4ef8f06bab73a5a0c9f97d2fd
MBail1 <michaelbailey@markerstudy.com>
2025-07-09 10:39:00 +0100

    Setting headless back to true

--- tests/Playwright.Tests/BasePlaywrightTest.cs
+++ tests/Playwright.Tests/BasePlaywrightTest.cs
@@ -15,7 +15,7 @@ public abstract class BasePlaywrightTest : PageTest
     [SetUp]
     public async Task SetUp()
     {
-        Browser = await Playwright.Chromium.LaunchAsync(new() { Headless = false });
+        Browser = await Playwright.Chromium.LaunchAsync(new() { Headless = true });
         Context = await Browser.NewContextAsync(new() { Locale = "en-GB" });
         Context.SetDefaultTimeout(60000);
         Page = await Context.NewPageAsync();
